eb023ea6a0381696128dad4ca829b3d00be653ea
Erik <erik@erikolsson.dev>
2020-01-23 02:44:47 +0100

    Secure websockets?

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `https://telegram-clone-gql-prod.herokuapp.com/graphql`;
-export const wsEndpoint = `ws://telegram-clone-gql-prod.herokuapp.com/graphql`;
+export const wsEndpoint = `wss://telegram-clone-gql-prod.herokuapp.com/graphql`;
