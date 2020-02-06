653822df46f78f3b8390c10af436b7d83d667e4d
Erik <erik@erikolsson.dev>
2020-02-06 18:47:21 +0100

    Add styles

---
+++ frontend/src/styles/UserBadge.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const UserBadge = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: ${props => props.size === "small" ? "2rem" : "4rem"};
+    width: ${props => props.size === "small" ? "2rem" : "4rem"};
+    background-color: ${props => props.color};
+    border-radius: 50%;
+    color: ${props => props.theme.text};
+    font-weight: bold;
+    font-size: ${props => props.size === "small" ? "0.8rem" : "1.3rem"};
+`;
+
+export default UserBadge;
