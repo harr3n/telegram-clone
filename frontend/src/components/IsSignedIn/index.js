3fa734d90db95f85edae273157000ca1075d63f3
Erik Olsson <erik@erikolson.dev>
2020-05-25 18:47:53 +0200

    Use loader

--- frontend/src/components/IsSignedIn/index.js
+++ frontend/src/components/IsSignedIn/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { ME_QUERY } from "../../api/queries";
 import SignInSignUp from "../SignInSignUp";
+import Loading from "../Loading"
 
 const IsSignedIn = props => {
   const { data, loading, error } = useQuery(ME_QUERY);
   
-  if (loading) return <p>loading...</p>
+  if (loading) return <Loading />
   if(error || !data.me) return <SignInSignUp />
   return props.children
 };
